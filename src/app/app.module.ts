f59903cd22aa22c6abc4320900f75b7f5655f313
bortale <alebortna@gmail.com>
2021-10-16 21:48:51 +0200

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewLesson1Component } from './new-lesson1/new-lesson1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewLesson1Component
   ],
   imports: [
     BrowserModule
